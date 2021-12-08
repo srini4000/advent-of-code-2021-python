b8978cb21ce19c59078aabb361a5907fafa84bc4
srini4000 <srini4000@gmail.com>
2021-12-08 17:27:02 -0600

    day06 part 2 completed

--- day06/day06.py
+++ day06/day06.py
@@ -3,40 +3,56 @@
 https://adventofcode.com/2021/day/6
 """
 from dataclasses import dataclass
+from collections import Counter
 
 
 @dataclass
 class State:
-    timers: list[int]
+    counter: Counter[int]
     days: int = 0
 
-    def add_day(self, days: int = 1):
+    def add_days(self, days: int = 1):
         for _ in range(days):
-            new_timers: list[int] = []
-            for index, timer in enumerate(self.timers):
+            new_counter: Counter[int] = Counter()
+            for timer, count in self.counter.items():
                 if timer == 0:
-                    self.timers[index] = 6
-                    new_timers.append(8)
+                    self.counter[0] -= count  # these 0 go away
+                    new_counter[6] += count  # 0 becomes 6
+                    new_counter[8] += count  # Also, new childs are added with life of 8
                 else:
-                    self.timers[index] -= 1
-            self.timers += new_timers
+                    self.counter[timer] -= count
+                    new_counter[timer - 1] += count
+            self.counter += new_counter
             self.days += 1
 
     def __len__(self):
-        return len(timers)
+        return sum(self.counter.values())
 
 
 if __name__ == "__main__":
     input_text = "3,4,3,1,2"
     timers = [int(timer) for timer in input_text.split(",")]
-    state = State(timers)
-    state.add_day(80)
+    counter = Counter(timers)
+    state = State(counter)
+    state.add_days(80)
+
+    part_1_answer = len(state)
     assert len(state) == 5934
+
+    state.add_days(256 - 80)
+    part_2_answer = len(state)
+    assert part_2_answer == 26984457539
 
     with open("day06/input.txt") as file:
         input_text = file.read()
         timers = [int(timer) for timer in input_text.split(",")]
-        state = State(timers)
-        state.add_day(80)
+        counter = Counter(timers)
+        state = State(counter)
+
+        state.add_days(80)
         part_1_answer = len(state)
         print(f"{part_1_answer = }")
+
+        state.add_days(256 - 80)
+        part_2_answer = len(state)
+        print(f"{part_2_answer = }")
